2557625f53457f236c40548cfc5d33efba85569d
Benjamin Bloomfield <bhb123@gmail.com>
2021-01-12 11:02:00 -0500

    fix(exsurge): Fix use of ℣ and ℟ for versicle, response symbols

--- src/components/Exsurge.tsx
+++ src/components/Exsurge.tsx
@@ -151,7 +151,8 @@ const Exsurge: React.FC<ExsurgeProps> = ({
     ctxt.specialCharProperties["font-variant"] = "normal";
     ctxt.specialCharProperties["font-size"] = "120%";
     ctxt.specialCharProperties["font-weight"] = "400";
-    ctxt.specialCharText = (char) => char.toLowerCase();
+    const defaultSpecialCharText = ctxt.specialCharText || ((char: string) => char);
+    ctxt.specialCharText = (char) => defaultSpecialCharText(char).toLowerCase();
     ctxt.textAfterSpecialChar = "";
     ctxt.setRubricColor("#d00");
     ctxt.minSpaceAboveStaff = 0;

d963790685f6fe67d0cad3340788a10e8f3fca62
Benjamin Bloomfield <bhb123@gmail.com>
2021-01-20 13:46:22 -0500

    fix(syllabified exsurge): Refactor syllabified exsurge, and add missing new parameter

--- src/components/SyllabifiedExsurge.tsx
+++ src/components/SyllabifiedExsurge.tsx
@@ -1,130 +1,27 @@
 import React from "react";
-import * as exsurge from "exsurge";
 import { GabcSyllabified } from 'gabc-utils';
 
-import Exsurge from './Exsurge';
-import { TextTypesOptions } from "../interfaces/TextTypeOptions";
+import Exsurge, { ExsurgeProps } from './Exsurge';
 
-interface SyllabifiedExsurgeProps {
+type SyllabifiedExsurgeProps = {
   text: string;
   notation: string;
   isEasterTime?: boolean;
-  capitalizeInitial?: boolean;
-  useDropCap?: boolean;
-  annotation?: string | string[];
-  contentEditable?: boolean;
-  alignment?: "english" | "latin";
-  width?: number;
-  height?: number;
-  zoom?: number;
-  selection?: exsurge.Selection;
-
-  id?: string;
-  style?: any;
-  className?: string;
-
-  supertitle?: string;
-  title?: string;
-  subtitle?: string;
-  textLeft?: string;
-  textRight?: string;
-
-  defaultFont?: string;
-  defaultColor?: string;
-  defaultTitleAlignment?: string;
-
-  font?: string;
-  baseFontSize?: number;
-  staffSize?: number;
-  interSyllabicSpacing?: number;
-  spaceBetweenSystems?: number;
-  spaceAboveLyrics?: number;
-
-  textStyles?: TextTypesOptions;
-
-  onScoreUpdate?(score: exsurge.ChantScore, gabceHeaderLen: number): any;
-  onKeyDown?(event: React.KeyboardEvent<HTMLDivElement>): any;
-}
+} & ExsurgeProps;
 
 const SyllabifiedExsurge: React.FC<SyllabifiedExsurgeProps> = ({
   text,
   notation,
   isEasterTime,
-  capitalizeInitial = true,
-  useDropCap,
-  annotation,
-  contentEditable,
-  alignment,
-  width,
-  height,
-  zoom,
-  selection,
-
-  id,
-  style,
-  className,
-
-  supertitle,
-  title,
-  subtitle,
-  textLeft,
-  textRight,
-
-  defaultFont,
-  defaultColor,
-  defaultTitleAlignment,
-
-  font,
-  staffSize,
-  baseFontSize,
-  interSyllabicSpacing,
-  spaceBetweenSystems,
-  spaceAboveLyrics,
-
-  textStyles,
-
-  onScoreUpdate,
-  onKeyDown,
+  gabc,
+  ...otherProps
 }: SyllabifiedExsurgeProps) => {
-  const gabc = GabcSyllabified.merge(text, notation, isEasterTime, capitalizeInitial);
+  gabc = GabcSyllabified.merge(text, notation, isEasterTime, otherProps.useDropCap);
 
   return (
     <Exsurge
       gabc={gabc}
-      useDropCap={useDropCap}
-      annotation={annotation}
-      contentEditable={contentEditable}
-      alignment={alignment}
-      width={width}
-      height={height}
-      zoom={zoom}
-      selection={selection}
-
-      id={id}
-      style={style}
-      className={className}
-
-      supertitle={supertitle}
-      title={title}
-      subtitle={subtitle}
-      textLeft={textLeft}
-      textRight={textRight}
-
-      defaultFont={defaultFont}
-      defaultColor={defaultColor}
-      defaultTitleAlignment={defaultTitleAlignment}
-
-      font={font}
-      staffSize={staffSize}
-      baseFontSize={baseFontSize}
-      interSyllabicSpacing={interSyllabicSpacing}
-      spaceBetweenSystems={spaceBetweenSystems}
-      spaceAboveLyrics={spaceAboveLyrics}
-
-      textStyles={textStyles}
-
-      onScoreUpdate={onScoreUpdate}
-      onKeyDown={onKeyDown}
+      {...otherProps}
     />
   )
 };
